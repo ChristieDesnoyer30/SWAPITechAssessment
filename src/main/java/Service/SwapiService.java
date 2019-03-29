a4e1cf45f95b667fc5a8950452fddfd371821f98
Christie Desnoyer <christiedesnoyer@Christies-MacBook-Pro.local>
2019-03-28 20:35:30 -0400

    Adds test for API response call to make sure it is returning the correct info by checking against the movie titles

--- src/main/java/Service/SwapiService.java
+++ src/main/java/Service/SwapiService.java
@@ -1,7 +1,23 @@
 package Service;
 
+import Model.AllMovieInfo;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class SwapiService {
+
+    public AllMovieInfo fetchMovieInfo(){
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.USER_AGENT, "Spring");
+
+        ResponseEntity<AllMovieInfo> response = restTemplate.exchange("https://swapi.co/api/films/2/?format=json",
+                HttpMethod.GET, new HttpEntity<>(headers), AllMovieInfo.class);
+        return response.getBody();
+    }
 }
