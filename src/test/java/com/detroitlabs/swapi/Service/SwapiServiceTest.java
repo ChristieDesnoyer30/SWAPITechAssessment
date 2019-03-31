57bb2f80fc1017fb3040a4dbdc8b6fcce8355eca
Christie Desnoyer <christiedesnoyer@Christies-MacBook-Pro.local>
2019-03-31 18:59:15 -0400

    Adding some Mockito testing to InjectMocks the SwapiService

--- src/test/java/com/detroitlabs/swapi/Service/SwapiServiceTest.java
+++ src/test/java/com/detroitlabs/swapi/Service/SwapiServiceTest.java
@@ -6,27 +6,36 @@ import com.detroitlabs.swapi.Model.CharacterDetails.Results;
 import com.detroitlabs.swapi.Model.MovieInfoAPI.AllMovieInfo;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-
 import static org.hamcrest.core.IsEqual.equalTo;
 
+@RunWith(MockitoJUnitRunner.class)
 public class SwapiServiceTest {
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @InjectMocks
+    private SwapiService swapiService;
 
     @Test
     public void shouldFetchMovieInfoForEmpireStrikesBack() {
-    SwapiService swapiService = new SwapiService();
+    swapiService = new SwapiService();
         AllMovieInfo allMovieInfo = swapiService.fetchMovieInfo();
-
         Assert.assertEquals("The Empire Strikes Back", allMovieInfo.getTitle());
     }
 
-@Test
+    @Test
     public void shouldFetchResultsFromCharacters(){
-    SwapiService swapiService = new SwapiService();
+    swapiService = new SwapiService();
      Results results = swapiService.fetchInfoByName("Luke Skywalker");
      Assert.assertEquals("male", results.get(0).getGender());
 }
@@ -43,7 +52,7 @@ public class SwapiServiceTest {
 
     @Test
     public void shouldFetchTatooineAsHomeworld(){
-        SwapiService swapiService = new SwapiService();
+        swapiService = new SwapiService();
        String actual =  swapiService.fetchPlanetName("https://swapi.co/api/planets/1/").getName();
 
      Assert.assertThat(actual, equalTo("Tatooine"));
